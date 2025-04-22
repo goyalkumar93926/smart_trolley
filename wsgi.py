c32d23a20ef3ae967c8bc39d79ee7b60cf88e092
Sheelam Goyal <goyalsheelam5@gmail.com>
2025-04-22 17:08:26 +0530

    Fix Eventlet monkey patch issue

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,16 @@
-from app import app, socketio
+import eventlet
+eventlet.monkey_patch()  # Make sure this is at the very top
 
-if __name__ == "__main__":
-    socketio.run(app)
+from flask import Flask
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=10000)
+
