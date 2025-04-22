3a6dc1d8fe3297ee6d5e00a60e952aec0ef86e31
Sheelam Goyal <goyalsheelam5@gmail.com>
2025-04-22 18:12:27 +0530

    Initial commit

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,10 @@
 import eventlet
-eventlet.monkey_patch()  # Make sure this is at the very top
+eventlet.monkey_patch()
 
-from flask import Flask
+from app import app
 from flask_socketio import SocketIO
 
-app = Flask(__name__)
 socketio = SocketIO(app)
 
-@app.route('/')
-def index():
-    return "Hello, World!"
-
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=10000)
-
+    socketio.run(app, host='0.0.0.0', port=5000)
